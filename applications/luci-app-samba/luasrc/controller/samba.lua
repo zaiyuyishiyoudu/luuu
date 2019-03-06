72283df7673fb1303a498aba3954f3a27420ca97
zaiyuyishiyoudu <401626436@qq.com>
2019-03-07 01:20:08 +0800

    luci

---
+++ applications/luci-app-samba/luasrc/controller/samba.lua
@@ -0,0 +1,15 @@
+-- Copyright 2008 Steven Barth <steven@midlink.org>
+-- Copyright 2008 Jo-Philipp Wich <jow@openwrt.org>
+-- Licensed to the public under the Apache License 2.0.
+
+module("luci.controller.samba", package.seeall)
+
+function index()
+	if not nixio.fs.access("/etc/config/samba") then
+		return
+	end
+
+	local page
+
+	entry({"admin", "nas", "samba"}, cbi("samba"), _("Network Shares"),10).dependent = true
+end
